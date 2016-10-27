574f5361f87165b2b9199dcdb508218a61033694
X00108966 <X00108966@computing.stu.it-tallaght.ie>
2016-10-27 10:55:45 +0100

    unit tests basic

---
+++ BankTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+namespace BankTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double startBalance = 12;
+            double debitAmount = 4;
+            double expected = 16;
+
+            BankAccount account = new BankAccount("Gareth Quirke", startBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
